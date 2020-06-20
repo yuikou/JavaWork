61f90f5213aa3217d9aa0fd9802a6af4bf8873b9
yuikou <yuikou777@gmail.com>
2020-06-20 13:07:40 +0800

    update Home Work5

--- HomeWork5.java
+++ HomeWork5.java
@@ -12,17 +12,18 @@ public class HomeWork5 {
 		double[][] doubleArray = { {1.2, 3.5, 2.2}, {7.4, 2.1, 8.2} };
 		HomeWork5 hw5 = new HomeWork5();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("請輸入寬:");
-		int w = sc.nextInt();
-		System.out.println("請輸入高:");
-		int h = sc.nextInt();
+//		System.out.println("請輸入寬:");
+//		int w = sc.nextInt();
+//		System.out.println("請輸入高:");
+//		int h = sc.nextInt();
 		
-		hw5.starSquare(w, h);	//Q1
-		//hw5.randAvg();	//Q2
+		//hw5.starSquare(w, h);	//Q1
+		hw5.randAvg();	//Q2
 		//hw5.randAvg2();	//Q2-2
 		//System.out.println(hw5.maxElement(intArray));
 		//System.out.println(hw5.maxElement(doubleArray));
 		//hw5.genAuthCode();	//Q5
+		hw5.getAuthCode2();
 	
 	}
 
@@ -45,20 +46,15 @@ public class HomeWork5 {
 		//Create 10 random number (0~100)
 		int[] rand = new int[10];
 		double sum = 0;
-		double avg;
+		
+		System.out.println("10個亂數為:");
 		for (int i = 0; i < rand.length; i++) {
-			int r = (int)(Math.random()*101);
-			rand[i] = r;
-		}
-		//Get average of the array
-		for (int i = 0; i< rand.length; i++) {
+			rand[i] = (int)(Math.random()*101);
+			System.out.print(rand[i] + " ");
 			sum += rand[i];
 		}
-		avg = sum / rand.length;
-		System.out.println("10個亂數為:");
-		for (int i : rand) {
-			System.out.printf("%d  ",i);
-		}System.out.printf("%n平均值為:%.2f ", avg);
+		
+		System.out.printf("%n平均值為:%.2f%n ", sum / rand.length);
 		
 	}
 	public void randAvg2() {
@@ -77,12 +73,12 @@ public class HomeWork5 {
 		System.out.println("The ten number is" + hs.toString());
 		
 		// Get average of the array
-		double avg, sum = 0;
+		double sum = 0;
 			for (int i = 0; i< random.length; i++) {
 				sum += random[i];
 			}
-			avg = sum / random.length;
-		System.out.printf("The average of 10 random number is [%.2f]", avg);
+			
+		System.out.printf("The average of 10 random number is [%.2f]", sum / random.length);
 		
 		}
 
@@ -119,6 +115,45 @@ public class HomeWork5 {
 	 */
 	
 	void genAuthCode() {
+			
+		char[] auth = new char[8];
+				
+		for(int i = 0; i < auth.length; i ++) {
+			int pattern = (int)((Math.random()*3)+1);
+			switch(pattern) {
+			//num (48 ~ 57 = 0 ~ 9) '0' in ASCII is 48
+			case 1:
+				auth[i] = (char)((Math.random()*10) + 48);
+				break;
+			//A-Z 'A' in ASCII is 65
+			case 2:
+				auth[i] = (char)((Math.random()*26) + 65);
+				break;
+			//a-z 'a' in ASCII is 97
+			case 3:
+				auth[i] = (char)((Math.random()*26) + 97);
+				break;
+			}
+		}
+	
+		System.out.print("Here is your captcha: ");
+		for (char row:auth) {
+			System.out.printf("%1s", row);
+		}
+		
+	}
+	
+	void getAuthCode2() {
+		//Our target char in ASCII code is [48-57,65-90,97-122]
+		int times = 0;
+		System.out.print("Here is your captcha: ");
+		while (times <= 8) {
+			char r = (char)((Math.random()*75) + 48);  //Set the number range between 48-122.
+			if (((r >= 48) && (r <= 57)) || ((r >= 65) && (r <= 90)) || ((r >= 97) && (r <= 122))) {
+				System.out.print(r + "");
+			}
+			times++;
+		}
 		
 	}
 
